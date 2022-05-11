a426775ad218e929900fcbc723e62cbdbb3c981e
gavin-shaw <gavin@gnarus.io>
2022-05-11 08:27:20 +0200

    chore: prep for deploy

--- db/init_mongo_db.py
+++ db/init_mongo_db.py
@@ -1,12 +1,12 @@
 from decouple import config
 from pymongo import DESCENDING, MongoClient
 
-MONGO_URL = config('MONGO_URL', default='mongodb://localhost:27017/')
+MONGO_URI = config('MONGO_URI', default='mongodb://localhost:27017/')
 MONGO_DB_NAME = config('MONGO_DB_NAME', default='pegaxy')
 
 
 def init_mongo_db():
-    client = MongoClient(MONGO_URL)
+    client = MongoClient(MONGO_URI)
     db = client[MONGO_DB_NAME]
     collection = db['contract_transactions']
     collection.create_index("hash", unique=True)
