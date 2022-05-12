1e7b198e5be2f0f0f0b6514e4c4ec120988fbcbd
hiki0505 <hikmet.pirmamedov@mail.ru>
2022-05-12 16:34:55 +0400

    chore: refactored parse_pegas into class and removed redundant imports and files

--- app/main_pegas.py
+++ app/main_pegas.py
@@ -1,16 +1,16 @@
 import asyncio
 import time
 
-from db.pg_db import PgDb
-from services.parse_pegas import parse_pegas
+
+from services.parse_pegas_cls import ParsePegas
 
 start_time = time.time()
 
 if __name__ == '__main__':
-    pg_db = PgDb()
 
+    parse_pegas = ParsePegas()
     # asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # Necessary for Windows users
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(parse_pegas(pg_db))
+    loop.run_until_complete(parse_pegas.parse_pegas())
     end_time = time.time() - start_time
     print(f"\nExecution time: {end_time} seconds")
